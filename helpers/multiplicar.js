f66d7de40045fb1255f23c010075d4c94a8429ff
Alexi Lobos <loboalexis2001@gmail.com>
2022-08-09 17:20:14 -0400

    Primer commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const colors = require('colors');
+
+const crearArchivo = async( base = 5, listar = false, hasta = 10) => {
+    
+    try {
+        
+        let salida = '';
+        let consola = '';
+
+        for (let i = 1; i <= hasta; i++) {
+            salida += `${ base } x ${ i } = ${ base * i }\n`;  // \n salto de linea
+            consola += `${ base } ${'x'.yellow} ${ i } ${'='.yellow} ${ base * i }\n`;  // \n salto de linea
+        }
+
+        if( listar) {
+            console.log('======================'.red);
+            console.log('   Tabla del:'.green, colors.blue( base ));
+            console.log('======================'.red);
+            console.log(consola);
+        }
+
+        fs.writeFileSync( `./salida/tabla-${base}.txt`, salida);
+
+        return `tabla-${base}.txt`;
+    } catch (err) {
+        throw err;
+    }
+
+}
+
+module.exports = {
+    crearArchivo
+} 
